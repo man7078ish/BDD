8c12f79f992667e8e4cb7de2b1d8c65c31a50da6
man7078ish <38471526+man7078ish@users.noreply.github.com>
2019-03-18 17:48:07 +0530

    Add files via upload

---
+++ BDD/resetButtonFunctionality/src/main/java/com/cg/version1/resetButtonFunctionality/ResetButtonFunctionalityApplication.java
@@ -0,0 +1,13 @@
+package com.cg.version1.resetButtonFunctionality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResetButtonFunctionalityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResetButtonFunctionalityApplication.class, args);
+	}
+
+}
---
+++ BDD/resetButtonFunctionality/src/test/java/com/cg/version1/resetButtonFunctionality/ResetButtonFunctionalityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cg.version1.resetButtonFunctionality;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResetButtonFunctionalityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
